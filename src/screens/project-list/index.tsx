165f58e48b08c1d6d8494683245b2f8e6a6aef31
xudianzhang <xudianzhang@geotmt.com>
2021-02-25 22:54:02 +0800

    jira

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react"
+import { SearchPanel } from './search-panel'
+import { List } from './list'
+import * as qs from 'qs'
+import { cleanObject, useDebounce, useMount } from "utils"
+
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const ProjectListScreen = () => {
+
+  const [param, setParam] = useState({
+    name: '',
+    personId: ''
+  })
+  const [list, setList] = useState([])
+  const [users, setUsers] = useState([])
+  const debouncedParam = useDebounce(param, 1000)
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`)
+    .then(async response => {
+      if (response.ok) {
+        setList(await response.json())
+      } 
+    })
+  }, [debouncedParam])
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async response => {
+      if (response.ok) {
+        setUsers(await response.json())
+      } 
+    })
+  })
+
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List list={list} users={users}/>
+    </div>
+  )
+}
