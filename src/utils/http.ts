3e0390e77d00fc1946d201807fad06472cda1e83
xudianzhang <469487939@qq.com>
2021-03-04 19:36:14 +0800

    fetch

---
+++ src/utils/http.ts
@@ -0,0 +1,40 @@
+import qs from 'qs'
+import * as Auth from 'auth-provider'
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+interface FetchConfig extends RequestInit {
+  data: object,
+  token: string
+}
+
+export const http = async (endpoint: string, { data, token, headers, ...customConfig }: FetchConfig) => {
+  const config = {
+    method: 'GET',
+    headers: {
+      Authorization: token ? `Bearer ${token}` : '',
+      'Content-Type': data ? 'application/json' : ''
+    },
+    ...customConfig
+  }
+
+  if (config.method.toUpperCase() === 'GET') {
+    endpoint += `?${qs.stringify(data)}`
+  } else {
+    config.body = JSON.stringify(data || {})
+  }
+  return window.fetch(`${apiUrl}/${endpoint}`, config)
+    .then(async response => {
+      if (response.status === 401) {
+        await Auth.logout()
+        window.location.reload()
+        return Promise.reject('请重新登录')
+      } 
+      const data = await response.json()
+      if (response.ok) {
+        return data
+      } else {
+        return Promise.reject(data)
+      }
+    })
+}
