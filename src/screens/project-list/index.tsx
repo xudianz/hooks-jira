55c343b24c5b6cf0869fda2abe92d82bbd984b4a
xudianzhang <469487939@qq.com>
2021-03-05 19:05:18 +0800

    token

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from "react"
 import { SearchPanel } from './search-panel'
 import { List } from './list'
-import * as qs from 'qs'
 import { cleanObject, useDebounce, useMount } from "utils"
-
-const apiUrl = process.env.REACT_APP_API_URL
+import { useHttp } from "utils/http"
 
 export const ProjectListScreen = () => {
-
   const [param, setParam] = useState({
     name: '',
     personId: ''
@@ -15,22 +12,15 @@ export const ProjectListScreen = () => {
   const [list, setList] = useState([])
   const [users, setUsers] = useState([])
   const debouncedParam = useDebounce(param, 500)
+  
+  const client = useHttp()
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`)
-    .then(async response => {
-      if (response.ok) {
-        setList(await response.json())
-      }
-    })
-  }, [debouncedParam])
+    client('projects', { data: cleanObject(debouncedParam)}).then(setList)
+  }, [debouncedParam, client])
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async response => {
-      if (response.ok) {
-        setUsers(await response.json())
-      } 
-    })
+    client('users').then(setUsers)
   })
 
   return (
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,14 +1,15 @@
 import qs from 'qs'
 import * as Auth from 'auth-provider'
+import { useAuth } from 'context/auth-context';
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 interface FetchConfig extends RequestInit {
-  data: object,
-  token: string
+  data?: object,
+  token?: string
 }
 
-export const http = async (endpoint: string, { data, token, headers, ...customConfig }: FetchConfig) => {
+export const http = async (endpoint: string, { data, token, headers, ...customConfig }: FetchConfig = {}) => {
   const config = {
     method: 'GET',
     headers: {
@@ -23,7 +24,7 @@ export const http = async (endpoint: string, { data, token, headers, ...customCo
   } else {
     config.body = JSON.stringify(data || {})
   }
-  return window.fetch(`${apiUrl}/${endpoint}`, config)
+  return window.fetch(`/${apiUrl}/${endpoint}`, config)
     .then(async response => {
       if (response.status === 401) {
         await Auth.logout()
@@ -37,4 +38,12 @@ export const http = async (endpoint: string, { data, token, headers, ...customCo
         return Promise.reject(data)
       }
     })
+}
+
+export const useHttp = () => {
+  const { user } = useAuth()
+  return (...[endpoint, config]: Parameters<typeof http>) => http(endpoint, {
+    ...config,
+    token: user?.token
+  })
 }
